15d4e940480cd7ad30b11a6ba277501b2822cf3a
Dipesh Mahato <dipeshmahato@Dipeshs-MacBook-Air.local>
2026-08-01 23:36:41 +0545

    firebase

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,7 +1,27 @@
+// const { initializeApp, cert } = require("firebase-admin/app");
+// const { getMessaging } = require("firebase-admin/messaging");
+
+// const serviceAccount = require("../serviceAccountKey.json");
+
+// const app = initializeApp({
+//   credential: cert(serviceAccount),
+// });
+
+// const messaging = getMessaging(app);
+
+// module.exports = {
+//   app,
+//   messaging,
+// };
+
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getMessaging } = require("firebase-admin/messaging");
 
-const serviceAccount = require("../serviceAccountKey.json");
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 const app = initializeApp({
   credential: cert(serviceAccount),
